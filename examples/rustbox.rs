2cc0bf809bfda90c549a1a2bfe7b5157bc8d3971
Jacob Mitchell <jmitchell@member.fsf.org>
2016-02-15 09:58:44 -0800

    Generalize border-drawing to any rectangular area.

--- examples/rustbox.rs
+++ examples/rustbox.rs
@@ -5,17 +5,42 @@ use std::default::Default;
 use rustbox::{Color, Style, RustBox};
 use rustbox::Key;
 
-fn draw_border(rb: &RustBox, sty: Style, fg: Color, bg: Color) {
+struct Rectangle {
+    x_min: usize,
+    y_min: usize,
+    x_max: usize,
+    y_max: usize,
+}
+
+impl Rectangle {
+    fn new(a: usize, b: usize, c: usize, d: usize) -> Rectangle {
+        Rectangle {
+            x_min: a,
+            y_min: b,
+            x_max: c,
+            y_max: d,
+        }
+    }
+}
+
+fn draw_border(rb: &RustBox, rect: Rectangle, sty: Style, fg: Color, bg: Color) {
     let horiz = '-';
     let vert = '|';
-    for x in 0..rb.width() {
-        rb.print_char(x, 0, sty, fg, bg, horiz);
-        rb.print_char(x, rb.height()-1, sty, fg, bg, horiz);
+    let corner = '+';
+
+    for x in rect.x_min..(rect.x_max+1) {
+        rb.print_char(x, rect.y_min, sty, fg, bg, horiz);
+        rb.print_char(x, rect.y_max, sty, fg, bg, horiz);
     }
-    for y in 0..rb.height() {
-        rb.print_char(0, y, sty, fg, bg, vert);
-        rb.print_char(rb.width()-1, y, sty, fg, bg, vert);
+    for y in rect.y_min..(rect.y_max+1) {
+        rb.print_char(rect.x_min, y, sty, fg, bg, vert);
+        rb.print_char(rect.x_max, y, sty, fg, bg, vert);
     }
+
+    rb.print_char(rect.x_min, rect.y_min, sty, fg, bg, corner);
+    rb.print_char(rect.x_max, rect.y_min, sty, fg, bg, corner);
+    rb.print_char(rect.x_max, rect.y_max, sty, fg, bg, corner);
+    rb.print_char(rect.x_min, rect.y_max, sty, fg, bg, corner);
 }
 
 fn main() {
@@ -28,7 +53,9 @@ fn main() {
     rustbox.print(1, 3, rustbox::RB_BOLD, Color::White, Color::Black,
                   "Press 'q' to quit.");
 
-    draw_border(&rustbox, rustbox::RB_NORMAL, Color::White, Color::Blue);
+    draw_border(&rustbox,
+                Rectangle::new(0, 0, rustbox.width()-1, rustbox.height()-1),
+                rustbox::RB_NORMAL, Color::White, Color::Blue);
 
     loop {
         rustbox.present();
